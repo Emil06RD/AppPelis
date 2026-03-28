ab36a7eddaed0198683c20f6333f1449bcf70fdb
Emil06RD <emil.montillard@gmail.com>
2026-03-27 22:24:52 -0400

    correcion mensajes en modo guest

--- src/routes/messageRoutes.js
+++ src/routes/messageRoutes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const messageController = require('../controllers/MessageController');
 const { requireAuth, requireEditor } = require('../middleware/auth');
 
-router.get('/', messageController.getIndex);
-router.get('/api/mine', messageController.getMyMessages);
+router.get('/', requireEditor, messageController.getIndex);
+router.get('/api/mine', requireEditor, messageController.getMyMessages);
 router.post('/create/:target', requireEditor, messageController.postCreate);
 router.get('/delete/:target/:id', requireEditor, messageController.getDelete);
 
